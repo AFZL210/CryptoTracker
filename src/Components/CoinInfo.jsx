d08cd9683de41afbd83c4dd8b97f652267328772
Mohd Afzal Khan <79896602+AFZL210@users.noreply.github.com>
2023-02-15 21:28:56 +0530

    added line chart

--- src/Components/CoinInfo.jsx
+++ src/Components/CoinInfo.jsx
@@ -1,10 +1,32 @@
-import React,{ useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { HistoricalChart } from '../config/api'
 import { CryptoState } from '../CryptoContext'
 import axios from 'axios'
 import { createTheme, makeStyles, ThemeProvider } from '@material-ui/core'
 import ReactLoading from 'react-loading';
+import 'chart.js/auto';
 import { Line } from 'react-chartjs-2'
+import SelectButton from './SelectButton'
+
+const chartDays = [
+  {
+    label: "24 Hours",
+    value: 1,
+  },
+  {
+    label: "30 Days",
+    value: 30,
+  },
+  {
+    label: "3 Months",
+    value: 90,
+  },
+  {
+    label: "1 Year",
+    value: 365,
+  },
+];
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -25,22 +47,24 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const CoinInfo = ({coin}) => {
+const CoinInfo = ({ coin }) => {
+  const [flag, setFlag] = useState(false)
   const classes = useStyles()
   const [historyData, setHistoryData] = useState()
   const [days, setDays] = useState(1)
 
-  const { currency } = CryptoState()
+  const { currency, setCurrency } = CryptoState()
 
-  const getHistoricalData = async() => {
-    const { data } = await axios.get(HistoricalChart(coin.id,days,currency.toLowerCase()))
+
+  const getHistoricalData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency.toLowerCase()))
     setHistoryData(data.prices)
   }
 
   console.log(historyData)
   useEffect(() => {
     getHistoricalData()
-  }, [currency, days])
+  }, [currency, days, historyData])
 
   const darkTheme = createTheme({
     palette: {
@@ -50,19 +74,74 @@ const CoinInfo = ({coin}) => {
       type: 'dark'
     }
   })
+  setTimeout(() => {
+    if (historyData === undefined) setCurrency(currency)
+  }, 1000)
 
   return (
     <ThemeProvider theme={darkTheme}>
       <div className={classes.container}>
-          {
-            !historyData? (
-              <ReactLoading type={'cylon'} color={'red'} height={100} width={100} />
-            ): (
-              <>
-                
-              </>
-            )
-          }
+        {
+          !historyData ? (
+            <ReactLoading type={'cylon'} color={'red'} height={100} width={100} />
+          ) : (
+            <>
+              <Line
+                data={{
+                  labels: historyData.map((coin) => {
+                    let date = new Date(coin[0]);
+                    let time =
+                      date.getHours() > 12
+                        ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                        : `${date.getHours()}:${date.getMinutes()} AM`;
+                    return days === 1 ? time : date.toLocaleDateString();
+                  }),
+
+                  datasets: [
+                    {
+                      data: historyData.map((coin) => coin[1]),
+                      label: `Price ( Past ${days} Days ) in ${currency}`,
+                      borderColor: "#EEBC1D",
+                    },
+                  ],
+                }}
+                options={{
+                  elements: {
+                    point: {
+                      radius: 1,
+                    },
+                  },
+                }}
+              />
+
+
+              <div
+                style={{
+                  display: "flex",
+                  marginTop: 20,
+                  justifyContent: "space-around",
+                  width: "100%",
+                }}
+              >
+                {chartDays.map((day) => (
+                  <SelectButton
+                    key={day.value}
+                    onClick={() => {
+                      setDays(day.value);
+                      setFlag(false);
+                    }}
+                    selected={day.value === days}
+                  >
+                    {day.label}
+                  </SelectButton>
+                ))}
+              </div>
+            </>
+
+          )
+
+
+        }
       </div>
     </ThemeProvider>
   )
--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <CryptoContext>
         <App />
       </CryptoContext>
     </BrowserRouter>
-  </React.StrictMode>
 );
